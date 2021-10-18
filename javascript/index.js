b3f11fd913bda82d9f61520660411d626d5d2313
fobadara <fobadara@gmail.com>
2021-10-18 15:58:27 +0100

    Create local storage

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line max-classes-per-file
+let books = JSON.parse(localStorage.getItem('books'));
+localStorage.setItem('books', JSON.stringify(books));
+let storedBooks = JSON.parse(localStorage.getItem('books'));
+
+class Library {
+  
+}
